6630605722b5476d0d4e74678a23d6567bb06cdd
Kseniia Klimenko <d17123220@mytudublin.ie>
2020-11-22 18:14:01 +0000

    Added spawing player tower and alien's fleet

---
+++ GE1 Assignment Project/Assets/Scripts/FleetBuilder.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Build alien fleet out of placeholder objects
+*/
+public class FleetBuilder : MonoBehaviour
+{
+    // Define prefabricated placeholders
+    public GameObject smallAlien;
+    public GameObject mediumAlien;
+    public GameObject bigAlien;
+    public GameObject motherShip;
+    public float prefabsize = 1.0f;
+    public static float saucerWidth = 12.0f;
+    public static float saucerDepth = 6.0f; 
+    public static float saucerHeight = 8.0f;   
+    public float horizontalSpacer = saucerWidth / 2;
+    public float verticalSpacer = saucerHeight;
+    public float depthSpacer = saucerDepth;
+    
+    // Define local constants
+    private const int SAUCER_SMALL = 0;
+    private const int SAUCER_MEDIUM = 1;
+    private const int SAUCER_BIG = 2;
+    private const int MOTHER_SHIP = 3;
+
+    // define initial movement speed in bricks per second
+    public float moveSpeed = 3;
+    public bool ready = false;
+    
+    // define fleet dimentions
+    public int fleetWidth = 11;
+    public int fleetDepth = 5;
+
+    // define how alien saucers are located in the fleet
+    public int[] fleetHeight = {SAUCER_BIG, SAUCER_BIG, SAUCER_MEDIUM, SAUCER_MEDIUM, SAUCER_SMALL};
+
+    private Vector3 CalculateRelativePosition(int i, int j, int k)
+    {
+        // vertical coordinate Y
+        float fullY = fleetHeight.Length;
+        float Y = (float) ((-1) * fullY + i) * saucerHeight + ((-1) * (fullY-1) +i ) * verticalSpacer;
+        
+        // forward coordinate Z
+        float halfZ = fleetDepth / 2;
+        float halfSpaceZ = (fleetDepth - 1) / 2;
+        float Z = (float) ((-1) * halfZ + j) * saucerDepth + ((-1) * halfSpaceZ + j ) * depthSpacer;
+
+        // horizontal coordinate X
+        float halfX = fleetWidth / 2;
+        float halfSpaceX = (fleetWidth - 1) / 2;
+        float X = (float) ((-1) * halfX + k) * saucerWidth + ((-1) * halfSpaceX + k ) * horizontalSpacer;       
+        
+        return new Vector3(X,Y,Z);
+    }
+
+    // coroutine to build 
+    System.Collections.IEnumerator BuildFleet()
+    {
+        // Build level by level
+        int i = 0;
+        foreach (int saucer in fleetHeight)
+        {
+            for (int k=0; k<fleetWidth; k++)
+            {
+                for (int j=0; j<fleetDepth; j++)
+                {
+                    // Calculate position of each new saucer
+                    Vector3 position = CalculateRelativePosition(i, j, k);
+
+                    // getSaucerType
+                    GameObject prefab = null;
+                    switch (saucer)
+                    {
+                        case SAUCER_SMALL: 
+                            prefab = smallAlien; 
+                            break;
+                        case SAUCER_MEDIUM: 
+                            prefab = mediumAlien;
+                            break;
+                        case SAUCER_BIG: 
+                            prefab = bigAlien;
+                            break;
+                        case MOTHER_SHIP: 
+                            prefab = motherShip;
+                            break;
+                    }
+
+                    // Instantiate object
+                    GameObject newSaucer = GameObject.Instantiate<GameObject>(prefab);
+
+                    // Set it as a child of this object
+                    newSaucer.transform.parent = this.transform;
+
+                    // Set new position
+                    newSaucer.transform.localPosition = position;
+
+                    // Rotate to parent rotation
+                    newSaucer.transform.rotation = Quaternion.LookRotation(transform.forward, transform.up);
+                }
+                
+                // Calculate total number of saucers
+                int numSaucers = fleetHeight.Length * fleetWidth; //* fleetDepth
+                
+                // wait between spawning saucers. Total wait no more than 2 seconds 
+                yield return new WaitForSeconds(2.0f / numSaucers);
+            }
+
+            i++;
+        }
+        ready = true;
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // before start alien fleet, choose rotation as random angle around Y axis
+        transform.rotation = Quaternion.LookRotation(new Vector3(Random.Range(-100,101), 0, Random.Range(-100,101)), transform.up);
+
+        // generate fleet using co-routine
+        StartCoroutine(BuildFleet());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move fleet
+        
+    }
+}
---
+++ GE1 Assignment Project/Assets/Scripts/PlayerBuilder.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+    Build a player tower out of DefenderCubes
+*/
+public class PlayerBuilder : MonoBehaviour
+{
+    // define prefabricated object - DefenderCube
+    public GameObject prefab;
+    public float prefabsize = 1.0f;
+
+    // define initial movement speed in bricks per second
+    public float moveSpeed = 5;
+
+    // Blueprint of player's tower. 3-d model build of cubes, 6x7x11
+    public int[,,] blueprint = new int[,,]
+    {
+        {
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1}           
+        },
+        {
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1}           
+        },
+        {
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1}           
+        },
+        {
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,1,1,1,1,1,1,1,1,1,0},
+            {0,1,1,1,1,1,1,1,1,1,0},
+            {0,1,1,1,1,1,1,1,1,1,0},
+            {0,1,1,1,1,1,1,1,1,1,0},
+            {0,1,1,1,1,1,1,1,1,1,0},
+            {0,0,0,0,0,0,0,0,0,0,0}
+        },
+        {
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,1,0},
+            {0,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0}
+        },
+        {
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,1,1,1,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0}
+        }
+    };
+
+    public bool destroyed = false;
+    public bool ready = false;
+
+    private Vector3 CalculateRelativePosition(int i, int j, int k)
+    {
+        // vertical coordinate Y - One cube down + another cube for each additional i 
+        float fullY = blueprint.GetLength(0);
+        float Y = (float) (-0.5 - fullY + i) * prefabsize ; 
+        // forward coordinate Z
+        float halfZ = blueprint.GetLength(2) / 2;
+        float Z = (float) ((-1) * halfZ + k + 0.5) * prefabsize;
+        // horizontal coordinate X
+        float halfX = blueprint.GetLength(1) / 2;
+        float X = (float) ((-1) * halfX + j + 0.5) * prefabsize;
+        
+        return new Vector3(X,Y,Z);
+    }
+
+
+
+    // Coroutine to build player's tower
+    System.Collections.IEnumerator BuildTower()
+    {
+        // wait initially 2 seconds
+        yield return new WaitForSeconds(2);
+        
+        // create player's tower based on blueprint
+        // each cube is size one on all edges, and everything is built below point of script
+
+        for (int i=0; i<blueprint.GetLength(0); i++)
+        {
+            for (int j=0; j<blueprint.GetLength(1); j++)
+            {
+                for (int k=0; k<blueprint.GetLength(2); k++)
+                {
+                    // Do it only if cube in this part of the model defined            
+                    if (blueprint[i,j,k] == 1)
+                    {
+                        // Calculate coordinates
+                        Vector3 position = CalculateRelativePosition(i,j,k);
+                        // Instantiate object
+                        GameObject newCube = GameObject.Instantiate<GameObject>(prefab);
+
+                        // Set it as a child of this object
+                        newCube.transform.parent = this.transform;
+
+                        // Set new position
+                        newCube.transform.localPosition = position;
+
+                        // Rotate to parent rotation
+                        newCube.transform.rotation = Quaternion.LookRotation(transform.forward, transform.up);
+                    }
+                }
+            }
+        }
+
+        // wait for additional second after building tower
+        yield return new WaitForSeconds(1);
+
+        // Move camera to the top of the tank
+        ready = true;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Use co-routine to build tower
+        StartCoroutine(BuildTower());
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // use controls to move tower around
+
+
+    }
+}
